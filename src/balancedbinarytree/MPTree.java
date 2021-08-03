15aca580aa1a6c55896b7ae44db72e2a732e0e56
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2021-08-03 17:50:57 +0800

    Completed the display methods.
    Fixed the display method so that not only does it work it also prints the root, the left tree and the right tree with only a single method call.

--- src/balancedbinarytree/MPTree.java
+++ src/balancedbinarytree/MPTree.java
@@ -1,90 +1,126 @@
 package balancedbinarytree;
 
 /**
- * 
- * @author Andrew Williamson / P113357
- * Date: 31/07/2021
- * Java 3 AT2 Q2
- * Question 2 – You are required to make a mechanical parts list sorted 
- * alphabetically. 
- * You need to create a balanced binary search tree for a dictionary
- * of no less than 10 words. 
- * You must be able to search the list, add and remove from the list.
+ *
+ * @author Andrew Williamson / P113357 Date: 31/07/2021 Java 3 AT2 Q2 Question 2
+ * – You are required to make a mechanical parts list sorted alphabetically. You
+ * need to create a balanced binary search tree for a dictionary of no less than
+ * 10 words. You must be able to search the list, add and remove from the list.
  */
-
 public class MPTree {
 
     private Part root;
 
-    // Constructor that accepts an integer for the value of the node.
-    public MPTree(String newPart) {
-        Part part = new Part(newPart);
-        root = part;
+    // Default constructor.
+    public MPTree() {
     }
-    
+
     // Public method to make sure the addPart() always starts at the root.
-    public void add(String newPart) {
-        addPart(root, newPart);
-    }
-    
-    // Add the new part into the tree if it's not there already.
-    private Part addPart(Part current, String newPart) {
-        if (current == null){
-            return new Part(newPart);
-        }
-        
-        if (newPart.compareToIgnoreCase(current.name) < 0) {
-            // Part comes before the current part, go to the left.
-            current.left = addPart(current.left, newPart);
-        } else if(newPart.compareToIgnoreCase(current.name) > 0) {
-            // Part comes after the current part, go to the right.
-            current.right = addPart(current.right, newPart);
+    public void add(String partName) {
+
+        Part part = new Part(partName);
+
+        if (root == null) {
+            root = part;
         } else {
-            // Part already exists.
+            addRecursive(root, part);
+        }
+
+    }
+
+    // Add the new part into the tree if it's not there already.
+    private Part addRecursive(Part current, Part newPart) {
+
+        if (current == null) {
+            current = newPart;
             return current;
         }
-        
+
+        if (newPart.name.compareToIgnoreCase(current.name) < 0) {
+            // Part comes before the current part, go to the left.
+            current.left = addRecursive(current.left, newPart);
+
+            //current = BalanceTree(current);
+        } else if (newPart.name.compareToIgnoreCase(current.name) > 0) {
+            // Part comes after the current part, go to the right.
+            current.right = addRecursive(current.right, newPart);
+
+            //current = BalanceTree(current);
+        }
         return current;
     }
-    
+
     public boolean containsPart(String partToSearch) {
         return searchForPart(root, partToSearch);
     }
-    
+
     private boolean searchForPart(Part current, String partToSearch) {
         if (current == null) {
             return false;
         }
-        
-        if(partToSearch.compareToIgnoreCase(current.name) > 0) {
+
+        if (partToSearch.compareToIgnoreCase(current.name) > 0) {
             return searchForPart(current.right, partToSearch);
-        } if (partToSearch.compareToIgnoreCase(current.name) < 0) {
+        }
+        if (partToSearch.compareToIgnoreCase(current.name) < 0) {
             searchForPart(current.left, partToSearch);
         }
         // the current one is the one your looking for.
-            return true;
-        
-        
+        return true;
     }
 
-    public Part delete(String partName){
+    public String Display() {
+        
+        if (root == null) {
+            return "There are no parts.";
+        }
+        String displayString = DisplayRoot() +
+                "\nLEFT TREE: " + DisplayTreeSide(root.left) +
+                "\nRIGHT TREE: " + DisplayTreeSide(root.right) ;
+        
+        return displayString;
+    }
+
+    private String DisplayTreeSide(Part current) {
+
+        String s = "";
+        if (current != null) {
+
+            s += current.name + ", ";
+            s += DisplayTreeSide(current.left);
+            s += DisplayTreeSide(current.right);
+
+        }
+        return s;
+    }
+
+    private String DisplayRoot() {
+
+        if (root == null) {
+            return "There is no root value.";
+        }
+        String a = "The root is: " + root.name;
+        return a;
+    }
+
+    public void delete(String partName) {
         deletePart(root, partName);
     }
 
     private Part deletePart(Part current, String partName) {
         if (current == null) {
-        return null;
-    }
-        
+            return null;
+        }
+
         if (partName.compareToIgnoreCase(current.name) == 0) {
             // Node to delete found
             // ... code to delete the node will go here
-            
+
             // Null if there are no children.
             if (current.left == null && current.right == null) {
                 return null;
             }
-            
+
             // If one is null return the other.
             if (current.left == null) {
                 return current.right;
@@ -92,25 +128,20 @@ public class MPTree {
             if (current.right == null) {
                 return current.left;
             }
-            
-            
-            
-            
-        } 
-        
+
+        }
+
         if (partName.compareToIgnoreCase(current.name) < 0) {
             current.left = deletePart(current.left, partName);
             return current;
         } else {
-        current.right = deletePart(current.right, partName);
+            current.right = deletePart(current.right, partName);
         }
-        
+
         return current;
-        
+
     }
 
-
-    
     // Method to find the total value of all the nodes in the tree.
     // Assuming all the node values are integers.
     public int FindSum(MPTree root) {
@@ -124,25 +155,23 @@ public class MPTree {
         }
     }
 
-    
-    public boolean IsTreeBalanced(MPTree root){
-        if (root == null){
+    public boolean IsTreeBalanced(MPTree root) {
+        if (root == null) {
             return true;
         }
         return (GetHeight(root) >= 1);
     }
-    
+
     public int GetHeight(MPTree node) {
-        
+
         // If the tree is empty then return 0
-        if (node == null){
+        if (node == null) {
             return 0;
         }
-        
+
         // Get the height of the left and right trees
         //int leftHeight = GetHeight(node.getLeft());
         //int rightHeight = GetHeight(node.getRight());
-        
         // If the left or right tree's heights are less than 0 or
         // the left tree height minus the right tree height is greater than 1
         // return -1 indecating the tree is unbalanced.
@@ -155,28 +184,27 @@ public class MPTree {
         
         // Return the higher number plus 1.
         return Math.max(leftHeight, rightHeight) + 1;
-        */
+         */
         // delete this.
         return 0;
     }
-    
-    
-    
+
     /**
      * Inner class for the part node's details.
      */
-    class Part{
+    class Part {
+
         String name;
         Part left;
         Part right;
-        
+
         // Constructor that accepts a String for the value of the node.
-    Part(String newPart) {
-        name = newPart;
-        left = null;
-        right = null;
-    }
-        
+        Part(String newPart) {
+            name = newPart;
+            left = null;
+            right = null;
+        }
+
     }
 
 }
--- src/balancedbinarytree/MechanicalPartsList.java
+++ src/balancedbinarytree/MechanicalPartsList.java
@@ -3,50 +3,39 @@ package balancedbinarytree;
 import java.util.LinkedList;
 
 /**
- * 
- * @author Andrew Williamson / P113357
- * Date: 31/07/2021
- * Java 3 AT2 Q2
- * Question 2 – You are required to make a mechanical parts list sorted 
- * alphabetically. 
- * You need to create a balanced binary search tree for a dictionary
- * of no less than 10 words. 
- * You must be able to search the list, add and remove from the list.
+ *
+ * @author Andrew Williamson / P113357 Date: 31/07/2021 Java 3 AT2 Q2 Question 2
+ * – You are required to make a mechanical parts list sorted alphabetically. You
+ * need to create a balanced binary search tree for a dictionary of no less than
+ * 10 words. You must be able to search the list, add and remove from the list.
  */
 public class MechanicalPartsList {
 
     public static void main(String[] args) {
-        
-        MPTree partsTree = new MPTree("root");
-        
-        partsTree.add("node1");
-        partsTree.add("node2");
-        
-        
-        partsTree.add("node3");
-        partsTree.add("node4");
-        
-        partsTree.add("node5");
-        partsTree.add("node6");
-        
-        partsTree.add("node7");
-        
-        partsTree.add("node8");
-        partsTree.add("node9");
-        
-        
+
+        MPTree partsTree = new MPTree();
+
+        partsTree.add("MAva");
+        partsTree.add("Zoe");
+        partsTree.add("David");
+        partsTree.add("William");
+        partsTree.add("Emma");
+        partsTree.add("Tiffany");
+        partsTree.add("Issac");
+        partsTree.add("Michael");
+        partsTree.add("Lily");
+        partsTree.add("Jill");
+        partsTree.add("Oliver");
+        partsTree.add("Ethan");
+        partsTree.add("Stacey");
+        partsTree.add("Becky");
+
         boolean isBalanced = partsTree.IsTreeBalanced(partsTree);
-        
-        
-        LinkedList<MPTree> parts = new LinkedList<>();
-        
-        
-        
-        
-        
-        
-        
-        
+
+        //LinkedList<MPTree> parts = new LinkedList<>();
+//        System.out.println(partsTree.DisplayRoot());
+        System.out.println(partsTree.Display());
+
     }
-    
+
 }
